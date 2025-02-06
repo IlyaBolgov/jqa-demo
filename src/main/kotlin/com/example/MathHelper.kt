f8654becbec8c620cfa0d21ac87937a720e7b59e
i.bolgov <bolgov.ilya19@gmail.com>
2025-02-06 21:15:35 +0300

    add MathHelperJavaInterop and refactor MathHelper for Java interop
    This commit introduces a new Java class `MathHelperJavaInterop` to demonstrate the usage of `MathHelper` functions. Additionally, `MathHelper` is refactored to include static methods with `@JvmStatic` annotations for better Java interoperability.

--- src/main/kotlin/com/example/MathHelper.kt
+++ src/main/kotlin/com/example/MathHelper.kt
@@ -1,22 +1,34 @@
 package com.example
 
-class MathHelper {
+class MathHelper private constructor() {
 
-    tailrec fun factorial(n: Int, acc: Int = 1): Int {
-        require(n >= 0) { "Factorial is not defined for negative numbers: n = $n" }
-        return if (n == 0) acc else factorial(n - 1, acc * n)
-    }
-
-    tailrec fun exponent(base: Double, power: Int, acc: Double = 1.0): Double {
-        if (base == 0.0 && power < 0) {
-            throw IllegalArgumentException("0 cannot be raised to a negative power.")
+    companion object {
+        @JvmStatic
+        fun factorial(n: Int): Int {
+            return factorial(n, 1)
         }
 
-        return when {
-            power == 0 -> acc
-            power > 0 -> exponent(base, power - 1, acc * base)
-            else       -> exponent(base, power + 1, acc / base)
+        @JvmStatic
+        tailrec fun factorial(n: Int, acc: Int = 1): Int {
+            require(n >= 0) { "Factorial is not defined for negative numbers: n = $n" }
+            return if (n == 0) acc else factorial(n - 1, acc * n)
+        }
+
+        @JvmStatic
+        fun exponent(base: Double, power: Int): Double {
+            return exponent(base, power, 1.0)
+        }
+
+        @JvmStatic
+        tailrec fun exponent(base: Double, power: Int, acc: Double = 1.0): Double {
+            if (base == 0.0 && power < 0) {
+                throw IllegalArgumentException("0 cannot be raised to a negative power.")
+            }
+            return when {
+                power == 0 -> acc
+                power > 0 -> exponent(base, power - 1, acc * base)
+                else -> exponent(base, power + 1, acc / base)
+            }
         }
     }
-
 }
